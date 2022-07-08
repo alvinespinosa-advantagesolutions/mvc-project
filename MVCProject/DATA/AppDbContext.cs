e98981da2e130870e0700e5164434672e187c705
Alvin Espinosa <AlvinE@magenic.com>
2022-07-08 14:18:15 +0800

    with dbContext

---
+++ MVCProject/DATA/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATA
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ MVCProject/MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DATA;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System.Diagnostics;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext appDbContext;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext appDbContext)
+        {
+            _logger = logger;
+            this.appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
